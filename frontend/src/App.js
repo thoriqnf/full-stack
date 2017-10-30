1d4381c039ab3590aac7c8d9729af2351415e623
thoriqnf <thoriqnfaizal@gmail.com>
2017-10-30 14:58:44 +0700

    display API data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+
+const API_URL = "https://backend-sikfttmpli.now.sh/";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="container">
+        <p>{API_URL}</p>
       </div>
     );
   }
