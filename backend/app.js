2582d59e3bdaecce4ef48066c24e841c0c8d6d17
thoriqnf <thoriqnfaizal@gmail.com>
2017-10-30 14:28:49 +0700

    install express and modified app.js

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//-----------------------------------------------------------------------------------------------
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+//-----------------------------------------------------------------------------------------------
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,12 +29,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
+//-----------------------------------------------------------------------------------------------
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+
+
+//-----------------------------------------------------------------------------------------------
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -44,3 +54,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+//-----------------------------------------------------------------------------------------------
