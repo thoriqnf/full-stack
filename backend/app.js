0838234e77d430ded4e9033c831e5ad6a36837a0
thoriqnf <thoriqnfaizal@gmail.com>
2017-10-30 14:32:17 +0700

    create dummy data at data.json

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,9 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+
+//-----------------------------------------------------------------------------------------------
+
 var app = express();
 
 //-----------------------------------------------------------------------------------------------
@@ -18,6 +21,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//-----------------------------------------------------------------------------------------------
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -25,6 +30,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//-----------------------------------------------------------------------------------------------
 
 app.use('/', index);
 app.use('/users', users);
@@ -52,6 +59,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+//-----------------------------------------------------------------------------------------------
 
 module.exports = app;
 
